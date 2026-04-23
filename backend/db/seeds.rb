c8d0424e570a75a800398e1840bb40609a1b77e1
Giovanni Alberto <delirable@gmail.com>
2026-04-23 16:11:36 -0600

    feat: add test user seed and wipe db

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create a test user
+User.find_or_create_by!(email: "test@nutripet.com") do |user|
+  user.password = "password123"
+  user.password_confirmation = "password123"
+end
+
+puts "Seeded 1 test user: test@nutripet.com / password123"
